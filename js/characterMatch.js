f9c2bb728d69d4153c5f91994c286092493026c9
Chris <chrisvolar@gmail.com>
2022-05-30 21:31:39 -0500

    works for 3 cases

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,9 @@
 exports.isCharacterMatch = function(string1, string2) {
+    let string1Change = string1.toLowerCase().split("").sort().join("");
+    let string2Change = string2.toLowerCase().split("").sort().join("");
+    return string1Change === string2Change;
+
 
 };
+
+console.log(exports.isCharacterMatch('abcde2', 'c2abed'))
